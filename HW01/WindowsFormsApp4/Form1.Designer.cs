189b305a0879b976c4276e760913c6ab287db4b6
9A713023 <73283855+9A713023@users.noreply.github.com>
2021-01-16 04:27:58 +0800

    Add files via upload

---
+++ HW01/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button.Font = new System.Drawing.Font("微軟正黑體", 19.69811F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button.ForeColor = System.Drawing.Color.Black;
+            this.button.Location = new System.Drawing.Point(595, 206);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(207, 247);
+            this.button.TabIndex = 0;
+            this.button.Text = "出一張";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(12, 12);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(577, 426);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picResult.TabIndex = 1;
+            this.picResult.TabStop = false;
+            this.picResult.Click += new System.EventHandler(this.picResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18.33962F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(626, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "迷因製造機";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HW01/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp4.Properties;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._01);
+            list.Add(Resources._02);
+            list.Add(Resources._03);
+            list.Add(Resources._04);
+            list.Add(Resources._05);
+            list.Add(Resources._06);
+            list.Add(Resources._07);
+            list.Add(Resources._08);
+            list.Add(Resources._09);
+            list.Add(Resources._10);
+
+
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+                poker poker1 = new poker();
+                List<int> poker = poker1.GetPoker(10);
+
+                string msg = "";
+                for (int i = 0; i < poker.Count; i++)
+                {
+                    msg += $"{poker[i]} ,";
+                }
+                picResult.Image = list[poker[0]];
+
+        }
+
+        private void picResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
